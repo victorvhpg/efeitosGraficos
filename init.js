f14ea51b3d408289f68940bd7e860136c30cea6c
Victor Hugo <victorvhpg@gmail.com>
2014-01-02 20:34:24 -0200

    ....

--- init.js
+++ init.js
@@ -6,48 +6,50 @@
     "use strict";
     var efeitos = {
 
+        realizarEfeitos: function(vet){
+            vet.forEach(function(item, indice){
+                efeitoGrafico.criar(item);
+            });
+        },
+
         criarEfeitos: function() {
+            var vet = [{
+                largura: 500,
+                altura: 200,
+                efeito: efeitoGrafico.efeitos.plasma,
+                container: document.querySelector("#efeito2")
+            }, {
+                largura: 500,
+                altura: 200,
+                efeito: efeitoGrafico.efeitos.plasmaComBuffer,
+                container: document.querySelector("#efeito3")
+            }, {
+                largura: 500,
+                altura: 200,
+                efeito: efeitoGrafico.efeitos.plasmaComBuffer,
+                posBuffer: 1,
+                container: document.querySelector("#efeito4")
+            }, {
+                largura: 500,
+                altura: 200,
+                efeito: efeitoGrafico.efeitos.plasmaComBuffer,
+                posBuffer: 2,
+                container: document.querySelector("#efeito5")
+            }];
+
             var img = new Image();
             img.addEventListener("load", function() {
-                var efeito1 = efeitoGrafico.criar({
+              vet.push({
                     img: img,
                     largura: 256,
                     altura: 134,
                     efeito: efeitoGrafico.efeitos.inverteCor,
                     container: document.querySelector("#efeito1")
                 });
+              efeitos.realizarEfeitos(vet);
             });
             img.src = "img.png";
-            //==============
-            var efeito2 = efeitoGrafico.criar({
-                largura: 500,
-                altura: 200,
-                efeito: efeitoGrafico.efeitos.plasma,
-                container: document.querySelector("#efeito2")
-            });
-            //=============
-            var efeito3 = efeitoGrafico.criar({
-                largura: 500,
-                altura: 200,
-                efeito: efeitoGrafico.efeitos.plasmaComBuffer,
-                container: document.querySelector("#efeito3")
-            });
-            //=============
-            var efeito4 = efeitoGrafico.criar({
-                largura: 500,
-                altura: 200,
-                efeito: efeitoGrafico.efeitos.plasmaComBuffer,
-                posBuffer: 1,
-                container: document.querySelector("#efeito4")
-            });
-            //=============
-            var efeito5 = efeitoGrafico.criar({
-                largura: 500,
-                altura: 200,
-                efeito: efeitoGrafico.efeitos.plasmaComBuffer,
-                posBuffer: 2,
-                container: document.querySelector("#efeito5")
-            });
+
         },
 
         init: function() {
